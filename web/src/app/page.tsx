f7adeb2b38c3affdac5d77b1d447c44acdc913ec
Arjun-G-04 <arjungirirajkumar@gmail.com>
2025-10-01 20:40:01 +0530

    feat: favicon, year fix, initiatives section

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,22 +1,24 @@
-import { getHighlights } from "@actions/cms";
+import { getHighlights, getInitiatives } from "@actions/cms";
 import { getUser } from "@actions/user";
+import Highlights from "@components/Landing/Highlights";
 import About from "@components/Landing/About";
+import Initiatives from "@components/Landing/Initiatives";
 import Footer from "@components/Landing/Footer";
 import Hero from "@components/Landing/Hero";
-import Highlights from "@components/Landing/Highlights";
 import Navbar from "@components/Landing/NavBar";
 
 export default async function Home() {
 	const user = await getUser();
-
 	const highlights = await getHighlights();
+	const initiatives = await getInitiatives();
 
 	return (
 		<div className="flex flex-col">
 			<Navbar isUser={user !== undefined} />
 			<Hero isUser={user !== undefined} />
-			<About />
 			<Highlights content={highlights} />
+			<About />
+			<Initiatives initiatives={initiatives} />
 			<Footer />
 		</div>
 	);
