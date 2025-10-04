499690bb80baad36b4896fe841eea983c139b40e
Ajay K M <katto.qt@gmail.com>
2025-10-04 14:57:20 +0530

    fix: navbar auth fix (#21)

--- web/src/app/projects/page.tsx
+++ web/src/app/projects/page.tsx
@@ -3,12 +3,14 @@ export const dynamic = "force-dynamic";
 import Navbar from "@components/Landing/NavBar";
 import Projects from "@components/Landing/Projects";
 import { getProjects } from "@actions/cms";
+import { getUser } from "@actions/user";
 
 export default async function Page() {
 	const entries = await getProjects();
+	const user = await getUser();
 	return (
 		<>
-			<Navbar isUser={false} />
+			<Navbar isUser={user !== undefined} />
 			<Projects entries={entries} />
 		</>
 	);
